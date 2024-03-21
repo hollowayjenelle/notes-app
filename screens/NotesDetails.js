5f9b09692e386b09980abe552dfb5d14e840a8fb
Jenelle Holloway <hollowayjenelle@gmail.com>
2024-03-21 17:25:59 -0500

    create NotesDetails page

---
+++ screens/NotesDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+import { globalStyles } from "../styles/global";
+
+const NotesDetails = () => {
+  return (
+    <View>
+      <Text>This is the notes details</Text>
+    </View>
+  );
+};
+
+export default NotesDetails;
