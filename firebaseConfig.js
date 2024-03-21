2b6e3c889885da5708c1b084e5d573148cc01ed3
Jenelle Holloway <hollowayjenelle@gmail.com>
2024-03-21 15:25:43 -0500

    setup firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "notes-app-db90e.firebaseapp.com",
+  projectId: "notes-app-db90e",
+  storageBucket: "notes-app-db90e.appspot.com",
+  messagingSenderId: "985706702515",
+  appId: "1:985706702515:web:9aabe4f399c9e85e0db2c6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
